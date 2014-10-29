c0de65b58bfb6599e2f05e665b128988c11b69dc
FikretPythonLesson <FikretPythonLesson@users.noreply.github.com>
2014-10-29 19:46:59 +0200

    Create SquareWithoutFunction.py
    Squares without using function

---
+++ SquareWithoutFunction.py
@@ -0,0 +1,28 @@
+import turtle
+
+for _ in range(4):
+    turtle.forward(50)
+    turtle.left(90)
+
+turtle.penup()
+turtle.forward(75)
+turtle.pendown()
+
+for _ in range(4):
+    turtle.forward(50)
+    turtle.left(90)
+
+turtle.penup()
+turtle.forward(75)
+turtle.pendown()
+
+for _ in range(4):
+    turtle.forward(50)
+    turtle.left(90)
+
+turtle.penup()
+turtle.forward(75)
+turtle.pendown()
+
+
+turtle.done()
